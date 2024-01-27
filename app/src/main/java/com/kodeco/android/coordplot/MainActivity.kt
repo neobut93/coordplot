2feb0400f750b5593408527b680c6eefbe9c2f1d
Vadim Butenko <vadim@Vadims-MacBook-Pro.local>
2024-01-27 14:39:46 -0800

    example

--- app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
+++ app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
@@ -3,12 +3,31 @@ package com.kodeco.android.coordplot
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kodeco.android.coordplot.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,8 +51,8 @@ fun PlotSurface() {
         //  This should include a Column composable that
         //  includes a Map, and two MapSlider composables
         //  (one slider for each axis).
+        Map()
     }
-
 }
 
 @Preview(showBackground = true)
@@ -45,17 +64,36 @@ fun PlotSurfacePreview() {
 }
 
 @Composable
-fun Map(xPercent: Float, yPercent: Float, modifier: Modifier = Modifier) {
-    // TODO fill out the square map here.
-    //  Create a Box composable with a size of 300.dp
-    //  and then create an inner Box composable
-    //  with a clip shape of CircleShape.
+fun Map(modifier: Modifier = Modifier) {
+    var xPercentage: Float by remember { mutableStateOf(0.5f) }
+    var yPercentage: Float by remember { mutableStateOf(0.5f) }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier
+                .size(300.dp)
+                .background(Color.Blue)
+        ) {
+            Box(
+                modifier
+                    .clip(shape = CircleShape)
+                    .background(Color.Green)
+                    .clip(shape = CircleShape)
+                    .size(36.dp)
+                    .offset((xPercentage * 300 - 18).dp, (yPercentage * 300 - 18).dp))
+        }
+            Slider(value = xPercentage, onValueChange = { xPercentage = it })
+            Spacer(modifier.size(20.dp))
+            Slider(value = yPercentage, onValueChange = { yPercentage = it })
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun MapPreview() {
     MyApplicationTheme {
-        Map(xPercent = 0.5f, yPercent = 0.5f)
+        Map()
     }
 }
