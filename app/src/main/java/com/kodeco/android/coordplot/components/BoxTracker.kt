72ef561b869d0ba237d4914db7a789a1992f9fca
vadim <neobut93@gmail.com>
2024-02-04 12:25:05 -0800

    added orientation support

--- app/src/main/java/com/kodeco/android/coordplot/components/BoxTracker.kt
+++ app/src/main/java/com/kodeco/android/coordplot/components/BoxTracker.kt
@@ -3,14 +3,19 @@ package com.kodeco.android.coordplot.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kodeco.android.coordplot.R
@@ -22,8 +27,11 @@ fun BoxTracker(xPercent: Float, yPercent: Float, modifier: Modifier = Modifier) 
     Box(
         modifier
             .size(Dimensions.squareBox.dp)
-            .background(Color.Cyan)
     ) {
+        Image(
+            modifier = Modifier
+                .fillMaxSize(),
+            painter = painterResource(id = R.drawable.back), contentDescription = "aaa")
         Box(
             modifier
                 .offset(
--- app/src/main/java/com/kodeco/android/coordplot/screens/MainScreen.kt
+++ app/src/main/java/com/kodeco/android/coordplot/screens/MainScreen.kt
@@ -1,10 +1,13 @@
 package com.kodeco.android.coordplot.screens
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,10 +15,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kodeco.android.coordplot.R
 import com.kodeco.android.coordplot.components.BoxTracker
 import com.kodeco.android.coordplot.components.MapSlider
@@ -23,30 +29,52 @@ import com.kodeco.android.coordplot.ui.theme.MyApplicationTheme
 
 @Composable
 fun MainScreen() {
-    Box(
-    ) {
-        var xPercentage: Float by remember { mutableStateOf(0.5f) }
-        var yPercentage: Float by remember { mutableStateOf(0.5f) }
+    val orientation = LocalConfiguration.current.orientation
+    var xPercentage: Float by remember { mutableStateOf(0.5f) }
+    var yPercentage: Float by remember { mutableStateOf(0.5f) }
+    Box {
         Image(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .blur(radius = 15.dp)
+            ,
             contentScale = ContentScale.Crop,
             painter = painterResource(id = R.drawable.sky), contentDescription = stringResource(id = R.string.app_background)
         )
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .fillMaxSize(),
-        ) {
-            BoxTracker(xPercent = xPercentage, yPercent = yPercentage)
-            MapSlider(xPercentage, yPercentage,
-                xValueChanged = { xValue ->
-                    xPercentage = xValue
-                },
-                yValueChanged = { yValue ->
-                    yPercentage = yValue
-                }
-            )
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Row(
+                modifier = Modifier
+                    .padding(30.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                BoxTracker(xPercent = xPercentage, yPercent = yPercentage)
+                MapSlider(xPercentage, yPercentage,
+                    xValueChanged = { xValue ->
+                        xPercentage = xValue
+                    },
+                    yValueChanged = { yValue ->
+                        yPercentage = yValue
+                    }
+                )
+            }
+        } else {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize(),
+            ) {
+                BoxTracker(xPercent = xPercentage, yPercent = yPercentage)
+                MapSlider(xPercentage, yPercentage,
+                    xValueChanged = { xValue ->
+                        xPercentage = xValue
+                    },
+                    yValueChanged = { yValue ->
+                        yPercentage = yValue
+                    }
+                )
+            }
         }
     }
 }
